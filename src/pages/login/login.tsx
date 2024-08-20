db0ea44ef446646079e3bcd7187be1655b7a4bff
quarerma <gabriel.oliveira.quaresma@gmail.com>
2024-08-20 11:44:00 -0300

    shadcn colors

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,8 +3,9 @@ import { loginschema, LoginSchema } from '../../schemas/login.schema';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '../../components/ui/button';
+import { Input } from '../../components/ui/input';
 
-export default function SignUpPage() {
+export default function LoginPage() {
   const {
     register,
     handleSubmit,
@@ -14,42 +15,39 @@ export default function SignUpPage() {
     resolver: zodResolver(loginschema),
   });
 
-  const inputClassName =
-    'rounded-[10px] w-full px-2 py-1 bg-[#3B3B3B] text-white placeholder:opacity-20 text-2xl border-none focus:outline-none focus:ring-2 focus:ring-[#BD9000] focus:ring-opacity-50';
-
   const onSubmit = (data: LoginSchema) => {
     console.log(data);
     reset();
   };
   return (
     <div className="w-screen h-screen">
-      <div className="w-screen text-white  bg-[#f7d770] h-[15vh] shadow-inner-custom">
+      <div className="w-screen text-primary-foreground  bg-primary h-[15vh] shadow-inner-custom">
         <h1
           style={{ textShadow: '0px 11px 8.3px rgba(0, 0, 0, 0.25)' }}
-          className="lg:text-[100px] h-full md:text-[70px] text-[50px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
+          className="lg:text-[90px] h-full  text-[70px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
         >
           CODEX
         </h1>
       </div>
-      <div className="min-h-[85vh] font-inter w-screen text-white bg-[#272727] flex   justify-center items-center">
-        <div className="bg-[#2C2C2C]  relative rounded-[30px] xl:w-2/12 lg:w-1/4 md:w-2/5  sm:w-1/2 w-full m-5  flex flex-col  items-center ">
+      <div className="min-h-[85vh] font-inter w-screen text-foreground bg-[#09090B] flex   justify-center items-center">
+        <div className="bg-[#0C0A09] border-2 border-border  relative rounded-[30px] xl:w-3/12 lg:w-1/4 md:w-2/5  sm:w-1/2 w-full m-5  flex flex-col  items-center ">
           <h1 className=" -top-5 absolute  w-full text-center md:text-4xl text-3xl">LOGIN</h1>
           <form
             onSubmit={handleSubmit(onSubmit)}
             className="px-4 w-full flex flex-col space-y-10 mt-16 mb-5  items-center"
           >
             <div className="w-full">
-              <input
+              <Input
                 autoComplete="false"
+                className="rounded-[15px]"
                 type="text"
-                className={`${inputClassName}`}
                 placeholder="Username"
                 {...register('username')}
               />
               <label className="text-red-500 text-sm">{errors.username?.message}</label>
             </div>
             <div className="w-full">
-              <input type="text" className={`${inputClassName}`} placeholder="Email" {...register('password')} />
+              <Input type="password" className="rounded-[15px]" placeholder="Email" {...register('password')} />
               <label className="text-red-500 text-sm">{errors.password?.message}</label>
             </div>
 
@@ -61,7 +59,7 @@ export default function SignUpPage() {
               Log In
             </Button>
           </form>
-          <div className="w-11/12 h-[1px] bg-[#3B3B3B] mb-5"></div>
+          <div className="w-11/12 h-[1px] bg-border mb-5"></div>
           <Link
             to={'/signup'}
             className=" hover:scale-110 duration-200 transition-transform  mb-5 text-center font-semibold w-[40%] text-2xl px-2 py-1 rounded-[10px]"
--- src/pages/signup/signup.tsx
+++ src/pages/signup/signup.tsx
@@ -2,6 +2,8 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import { signupSchema, SignUpSchema } from '../../schemas/signup.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from '../../components/ui/input';
+import { Button } from '../../components/ui/button';
 
 export default function SignUpPage() {
   const {
@@ -18,20 +20,18 @@ export default function SignUpPage() {
     reset();
   };
 
-  const inputClassName =
-    'rounded-[10px] w-full px-2 py-1 bg-[#3B3B3B] text-white placeholder:opacity-20 text-2xl border-none focus:outline-none focus:ring-2 focus:ring-[#BD9000] focus:ring-opacity-50';
   return (
-    <div className="w-screen h-screen">
-      <div className="w-screen text-white bg-[#f7d770] h-[15vh] shadow-inner-custom">
+    <div className="max-w-screen min-h-screen ">
+      <div className="max-w-screen text-primary-foreground  bg-primary h-[15vh] shadow-inner-custom">
         <h1
           style={{ textShadow: '0px 11px 8.3px rgba(0, 0, 0, 0.25)' }}
-          className="lg:text-[100px] h-full md:text-[70px] text-[50px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
+          className="lg:text-[90px] h-full  text-[70px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
         >
           CODEX
         </h1>
       </div>
-      <div className="min-h-[85vh] w-screen text-white bg-[#272727] flex  justify-center items-center">
-        <div className="bg-[#2C2C2C] relative rounded-[30px] xl:w-[22%] lg:w-[32%] md:w-2/5  sm:w-1/2 w-3/4 m-4 flex flex-col  items-center ">
+      <div className="min-h-[85vh] py-10 max-w-screen  font-inter text-white bg-[#09090B] flex  justify-center items-center">
+        <div className="bg-[#0C0A09] border border-border  relative rounded-[30px] xl:w-[27%] lg:w-[32%] md:w-2/5  sm:w-1/2 w-3/4 m-4 flex flex-col  items-center ">
           <h1 className=" -top-5 absolute  w-full text-center md:text-4xl text-3xl">SIGN-UP</h1>
 
           <form
@@ -39,33 +39,30 @@ export default function SignUpPage() {
             className="px-4 w-full flex flex-col space-y-12 mt-16 mb-5  items-center"
           >
             <div className="w-full">
-              <input type="text" className={`${inputClassName}`} placeholder="Username" {...register('username')} />
+              <Input type="text" placeholder="Username" className="rounded-[15px]" {...register('username')} />
               <label className="text-red-500 text-sm">{errors.username?.message}</label>
             </div>
             <div className="w-full">
-              <input type="text" className={`${inputClassName}`} placeholder="Email" {...register('email')} />
+              <Input type="email" placeholder="Email" {...register('email')} className="rounded-[15px]" />
               <label className="text-red-500 text-sm">{errors.email?.message}</label>
             </div>
             <div className="w-full">
-              <input type="text" className={`${inputClassName}`} placeholder="Password" {...register('password')} />
+              <Input type="text" placeholder="Password" className="rounded-[15px]" {...register('password')} />
               <label className="text-red-500 text-sm">{errors.password?.message}</label>
             </div>
             <div className="w-full">
-              <input
+              <Input
                 type="text"
-                className={`${inputClassName}`}
                 placeholder="Confirm Password"
+                className="rounded-[15px]"
                 {...register('confirmPassword')}
               />
               <label className="text-red-500 text-sm">{errors.confirmPassword?.message}</label>
             </div>
 
-            <button
-              type="submit"
-              className="bg-[#bd9d33] hover:bg-[#bd91007d] duration-200 transition-colors text-center font-semibold  text-2xl px-2 py-1 rounded-[10px]"
-            >
+            <Button type="submit" className="font-semibold  text-2xl px-2 py-1 rounded-[10px]">
               Create Account
-            </button>
+            </Button>
           </form>
           <div className="w-11/12 h-[1px] bg-[#3B3B3B] mb-5"></div>
           <div className=" mt-5 mb-10 text-2xl ">
