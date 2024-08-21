bf9fc60bf26cc911fa04d3b0362514c23d968ff5
Gabriel Oliveira Quaresma <gabriel.oliveira.quaresma@gmail.com>
2024-08-20 21:41:48 -0300

    in out component

---
+++ src/components/login-signup.tsx
@@ -0,0 +1,19 @@
+export default function LoginSignup({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="max-w-screen min-h-screen ">
+      <div className="max-w-screen text-primary-foreground  bg-primary h-[15vh] shadow-inner-custom">
+        <h1
+          style={{ textShadow: '0px 11px 8.3px rgba(0, 0, 0, 0.25)' }}
+          className="lg:text-[90px] h-full  text-[70px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
+        >
+          CODEX
+        </h1>
+      </div>
+      <div className="min-h-[85vh] py-10 max-w-screen  font-inter text-white bg-[#09090B] flex  justify-center items-center">
+        <div className="bg-[#0C0A09] border-4 border-border  relative rounded-[30px] xl:w-[27%] lg:w-[32%] md:w-2/5  sm:w-1/2 w-full m-4 flex flex-col  items-center ">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '../../components/ui/button';
 import { Input } from '../../components/ui/input';
+import LoginSignup from '../../components/login-signup';
 
 export default function LoginPage() {
   const {
@@ -20,54 +21,35 @@ export default function LoginPage() {
     reset();
   };
   return (
-    <div className="w-screen h-screen">
-      <div className="w-screen text-primary-foreground  bg-primary h-[15vh] shadow-inner-custom">
-        <h1
-          style={{ textShadow: '0px 11px 8.3px rgba(0, 0, 0, 0.25)' }}
-          className="lg:text-[90px] h-full  text-[70px] tracking-[0.3em] font-semibold flex items-center italic justify-center"
-        >
-          CODEX
-        </h1>
-      </div>
-      <div className="min-h-[85vh] font-inter w-screen text-foreground bg-[#09090B] flex   justify-center items-center">
-        <div className="bg-[#0C0A09] border-2 border-border  relative rounded-[30px] xl:w-3/12 lg:w-1/4 md:w-2/5  sm:w-1/2 w-full m-5  flex flex-col  items-center ">
-          <h1 className=" -top-5 absolute  w-full text-center md:text-4xl text-3xl">LOGIN</h1>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="px-4 w-full flex flex-col space-y-10 mt-16 mb-5  items-center"
-          >
-            <div className="w-full">
-              <Input
-                autoComplete="false"
-                className="rounded-[15px]"
-                type="text"
-                placeholder="Username"
-                {...register('username')}
-              />
-              <label className="text-red-500 text-sm">{errors.username?.message}</label>
-            </div>
-            <div className="w-full">
-              <Input type="password" className="rounded-[15px]" placeholder="Email" {...register('password')} />
-              <label className="text-red-500 text-sm">{errors.password?.message}</label>
-            </div>
-
-            <Button
-              type="submit"
-              className="font-semibold w-[40%] text-2xl px-2 py-1 rounded-[10px]"
-              variant={'default'}
-            >
-              Log In
-            </Button>
-          </form>
-          <div className="w-11/12 h-[1px] bg-border mb-5"></div>
-          <Link
-            to={'/signup'}
-            className=" hover:scale-110 duration-200 transition-transform  mb-5 text-center font-semibold w-[40%] text-2xl px-2 py-1 rounded-[10px]"
-          >
-            Sign Up
-          </Link>
+    <LoginSignup>
+      <h1 className=" -top-5 absolute font-bold w-full text-center md:text-4xl text-3xl">LOGIN</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="px-4 w-full flex flex-col space-y-10 mt-16 mb-5  items-center">
+        <div className="w-full">
+          <Input
+            autoComplete="false"
+            className="rounded-[15px]"
+            type="text"
+            placeholder="Username"
+            {...register('username')}
+          />
+          <label className="text-red-500 text-sm">{errors.username?.message}</label>
         </div>
-      </div>
-    </div>
+        <div className="w-full">
+          <Input type="password" className="rounded-[15px]" placeholder="Email" {...register('password')} />
+          <label className="text-red-500 text-sm">{errors.password?.message}</label>
+        </div>
+
+        <Button type="submit" className="font-semibold w-[40%] text-2xl px-2 py-1 rounded-[10px]" variant={'default'}>
+          Log In
+        </Button>
+      </form>
+      <div className="w-11/12 h-[1px] bg-border mb-5"></div>
+      <Link
+        to={'/signup'}
+        className=" hover:scale-110 duration-200 transition-transform  mb-5 text-center font-semibold w-[40%] text-2xl px-2 py-1 rounded-[10px]"
+      >
+        Sign Up
+      </Link>
+    </LoginSignup>
   );
 }
